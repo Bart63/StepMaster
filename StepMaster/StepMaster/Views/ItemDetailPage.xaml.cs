1b3bea3238dcb2a7752174e679f639d3e198b422
Bart63 <bartek.durys@gmail.com>
2021-10-21 07:19:24 +0200

    Start project from template

---
+++ StepMaster/StepMaster/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using StepMaster.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace StepMaster.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
