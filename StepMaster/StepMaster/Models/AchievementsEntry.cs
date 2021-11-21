2348d0f7a799984b5fd23b34173227d63c5201ae
SZYMON-PC\SzymonKlewicki <domeczekklewicki@gmail.com>
2021-11-21 19:45:39 +0100

    Achievements UI #5

---
+++ StepMaster/StepMaster/Models/AchievementsEntry.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace StepMaster.Models
+{
+    public class AchievementsEntry
+    {
+        public AchievementsEntry(string name, string description, bool isOwned, string iconName)
+        {
+            Name = name;
+            Description = description;
+            this.isOwned = isOwned;
+            Opacity = isOwned ? 1.0f : 0.5f;
+            IconName = iconName;
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool isOwned { get; set; }
+        public float Opacity { get; set; }
+        public string IconName { get; set; }
+        
+
+    }
+}
--- StepMaster/StepMaster/ViewModels/AchievementsViewModel.cs
+++ StepMaster/StepMaster/ViewModels/AchievementsViewModel.cs
@@ -1,13 +1,28 @@
-﻿using System;
+﻿using StepMaster.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace StepMaster.ViewModels
 {
     public class AchievementsViewModel : BaseViewModel
     {
+
+        public ObservableCollection<AchievementsEntry> AchievementsEntries { get; private set; }
+
         public AchievementsViewModel()
         {
+
+            AchievementsEntries = new ObservableCollection<AchievementsEntry>()
+            {
+                new AchievementsEntry("Pierwsze kroki", "Pokonaj pierwsze 5000 kroków.", true, "achievement_icon_3"),
+                new AchievementsEntry("Systematyczny", "Przez 5 dni pod rząd pokonaj min. 3000 kroków.", false, "achievement_icon_1"),
+                new AchievementsEntry("Zapaleniec", "W ciągu dnia pokonaj co najmniej 25000 kroków.", false, "achievement_icon_2"),
+                new AchievementsEntry("Mini zapaleniec", "W ciągu dnia pokonaj co najmniej 15000 kroków.", true, "achievement_icon_6"),
+                new AchievementsEntry("Niepokonany", "Przez 5 dni pod rząd wygrywaj rywalizację ze znajomym.", false, "achievement_icon_5"),
+                new AchievementsEntry("Gotowy do starcia", "Pokonaj znajomego w rywalizacji.", false, "achievement_icon_4")
+            };
 
         }
     }
